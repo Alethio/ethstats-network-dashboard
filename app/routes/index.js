ca064eae6ddeb1a61421cc6ef01a3e2a94da4643
Stefan Oancea <stefan.oancea93@gmail.com>
2019-05-07 15:45:08 +0300

    Remove Demo-Call-graphs route

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,14 +6,12 @@ import App from 'containers/App';
 
 import HistoricalBlock from 'routes/pages/HistoricalBlock';
 import NetworkStatistics from 'routes/pages/NetworkStatistics';
-import DemoCallGraphs from 'routes/pages/DemoCallGraphs';
 import PrivacyPolicy from 'routes/pages/PrivacyPolicy';
 
 export default () => {
   return (
     <Route path="/" component={App}>
       <IndexRoute name="Network Statistics" component={NetworkStatistics} />
-      <Route path="demo-call-graphs" component={DemoCallGraphs} />
       { PRIVACY_POLICY && <Route path="privacy-policy" component={PrivacyPolicy} /> }
       <Route path="history/block/:blockNr" component={({ params }) => <HistoricalBlock blockNr={params.blockNr}/>} />
       <Route path="*" name="404" component={() => {
