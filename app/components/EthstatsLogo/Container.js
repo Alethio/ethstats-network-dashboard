317989aa17559f1bb5d51eef926939bcb9ad0a0b
Stefan Oancea <stefan.oancea93@gmail.com>
2019-05-08 15:21:12 +0300

    Switch logos from header and footer

--- app/components/EthstatsLogo/Container.js
+++ app/components/EthstatsLogo/Container.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-  padding: 5px 26px 0 0 ;
+  padding: 5px 12px 0 0 ;
 `;
 
 export default Container;
--- app/components/EthstatsLogo/IconContainer.js
+++ app/components/EthstatsLogo/IconContainer.js
@@ -2,12 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const IconContainer = styled.div`
-  font-size: 20px;
-  padding: 3px 0 1px 7px;
-
-  &.explorer {
-    padding: 4px 0 0 7px;
-  }
+  font-size: 32px;
 `;
 
 export default IconContainer;
